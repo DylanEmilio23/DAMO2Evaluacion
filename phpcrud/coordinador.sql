47f2196732c38de186a195c5bad5da2f045fdda5
zetad <dylan.diaz14298@est.uca.edu.ni>
2023-03-29 01:53:42 -0600

    Actualizacion de php junto con la base de datos

---
+++ phpcrud/coordinador.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-03-2023 a las 09:45:51
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `myuca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `coordinador`
+--
+
+CREATE TABLE `coordinador` (
+  `idC` int(11) NOT NULL,
+  `nombres` varchar(11) NOT NULL,
+  `apellidos` varchar(11) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `facultad` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `coordinador`
+--
+
+INSERT INTO `coordinador` (`idC`, `nombres`, `apellidos`, `fechaNac`, `titulo`, `email`, `facultad`) VALUES
+(1, 'Dylan Emili', 'Diaz Jimene', '2027-11-03', 'Ingeniero en Sistema', 'dylan.diaz14298@est.uca.edu.ni', 'Facultad de Ciencia, Tecnología y Ambiente '),
+(2, 'Carlos Andr', 'Mendoza', '2018-02-04', 'Ingeniero en Sistema', 'carlos.mendoza13222@est.uca.edu.ni', 'Facultad de Ciencia, Tecnología y Ambiente ');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `coordinador`
+--
+ALTER TABLE `coordinador`
+  ADD PRIMARY KEY (`idC`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `coordinador`
+--
+ALTER TABLE `coordinador`
+  MODIFY `idC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
